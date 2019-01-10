f3c6b3eef650f6f69de3b99fdd56fb72f63a6dbe
french-fries-with-pepper <alexey.s.antonov@zoho.com>
2019-01-11 00:11:42 +0300

    add initGame and initBoard methods

--- main.js
+++ main.js
@@ -1,14 +1,20 @@
 const game = () => {
 	console.log("game start!");
 
+	//global things about size and destination here:
 	const boardSize = 4;
+	const clearCell = `<div class="cell"></div>\n`;
+	let board = document.getElementById("myGame");
 
+
+	// Main object for game board:
 	let mainBoard = {
 		size: boardSize,
 		isPossibleToPlay: true,
 		cellTable: [],
 
-		init: () =>{
+		// init game field
+		initGame: () =>{
 			for (let i = 0; i < mainBoard.size; i++) {
 				mainBoard.cellTable[i] = [];
 				for (var j = 0; j < mainBoard.size; j++) {
@@ -16,9 +22,31 @@ const game = () => {
 				}
 			}
 			console.log("initiolizing successful");
+		},
+
+		// mix game field array and html
+		// use grid layout 
+		initBoard: () =>{
+			let cellsInBoard = "";
+			
+			for (let i = 0; i < boardSize*boardSize; i++) {
+				cellsInBoard+=clearCell;
+			}
+
+			console.log("cells initiolizing successful");
+			
+			board.style['grid-template-columns'] = `repeat(${boardSize}, 1fr)`;
+			board.style['grid-template-rows'] = `repeat(${boardSize}, 1fr)`;
+			board.innerHTML = cellsInBoard;
 		}
+
 	}
-	mainBoard.init();
+
+
+
+
+	mainBoard.initGame();
+	mainBoard.initBoard();
 	console.log(mainBoard.cellTable);
 }
 
